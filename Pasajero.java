e73cb147757de9fae355270e62a2fdcfc738622a
tonadev <alejandro.garciaes@alumno.buap.mx>
2020-10-15 11:19:16 -0500

    Agrega el atributo faltante boleto de Pasajero, así como su respectivo getter, y constructor

--- Pasajero.java
+++ Pasajero.java
@@ -1,3 +1,14 @@
 public abstract class Pasajero {
+	protected Boleto boleto;
+
+	public Pasajero(Boleto boleto) {
+		this.boleto = boleto;
+	}
+
 	public abstract void documentarMaleta(Maleta maleta) throws Exception;
+
+	public Boleto getBoleto() {
+		return this.boleto;
+	}
 }
+
